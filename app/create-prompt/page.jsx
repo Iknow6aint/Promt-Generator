d8f9c65182793bc71083196655178c0278d8bd2d
iknow6aint <saintagbukor@gmail.com>
2023-09-14 23:05:37 +0100

    crate prompt

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -9,7 +9,29 @@ const CreatePrompt = () => {
     const [submitting, setIsSubmitting] = useState(false);
     const [post, setPost] = useState({ prompt: "", tag: "" });
 
-    const createPrompt = () => { }
+    const createPrompt = async (e) => {
+        e.preventDefault();
+        setIsSubmitting(true);
+
+        try {
+            const response = await fetch("/api/prompt/new", {
+                method: "POST",
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag,
+                }),
+            });
+
+            if (response.ok) {
+                router.push("/");
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsSubmitting(false);
+        }
+    };
 
     return (
         <Form
